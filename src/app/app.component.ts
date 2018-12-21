b9ce6fbf2c1b168f33656a91c18883f345eed385
Bharath Takkallapelly <tbharat2008@gmail.com>
2018-12-21 16:29:50 +0530

    Extacting images from video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Car Insurance';
+  @ViewChild('canvas') canvas; 
+
+  videoPath = '';
+  showVideo: boolean = false;
+  i = 0;
+
+  onFileUpload(event){
+    this.renderVideo(event.target.files[0]);
+    this.videoPath = '';
+    this.showVideo = false;
+    this.i = 0;
+    document.getElementById('thumbnailContainer').innerHTML = '';
+  }
+
+  renderVideo(file){
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (event) => {
+      this.videoPath = event.target['result'];
+      this.showVideo = true;
+    }
+  }
+
+
+  onVidLoad(event){
+    console.log(event);
+    setTimeout(() =>{
+      event.target.currentTime  = this.i;
+    },2000)
+    
+  }
+
+  onSeek(event){
+    console.log(event);
+    this.generateImg(this.i, event);
+    this.i+= 3;
+    if(this.i <= event.target.duration){
+      event.target.currentTime = this.i;
+    }
+  }
+
+  generateImg(i,e){
+    const canvas = this.canvas.nativeElement;
+    const context = canvas.getContext('2d');
+    context.drawImage(e.target, 0, 0, 250, 180);
+    const dataURL = canvas.toDataURL();
+
+    const img = new Image();
+    //img.crossOrigin="Anonymous";
+    img.setAttribute('src', dataURL);
+
+    document.getElementById('thumbnailContainer').appendChild(img);
+  }
 }
